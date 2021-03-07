06ec1caf2ac6df84542fe34c20387200752c2591
Seong-ran <66058005+Seong-ran@users.noreply.github.com>
2021-03-07 17:46:47 +0900

    CSS :: expantion ~ notice

--- js/index.js
+++ js/index.js
@@ -84,35 +84,15 @@ function progress() {
 
 // 섹션2 - 닌텐도 설명 슬라이드
 // 닌텐도 설명 슬라이드 관련 변수
-const slideList = document.querySelector('.slide__list');
-const slideContents = document.querySelectorAll('.slide__section');
-const slideBtnNext = document.querySelector('.slide__btn--next');
-const slideBtnPrev = document.querySelector('.slide__btn--prev');
-const slideLength = slideContents.length;
-const slideWidth = slideContents.getBoundingClientReact();
-const slideSpeed = 300; // slide speed
+let curPos = 0;
+let postion = 0;
+const IMAGE_WIDTH = 1000;
+const nextBtn = document.querySelector('.slide__btn--next');
+const prevBtn = document.querySelector('.slide__btn--prev');
+const images = document.querySelector(".slide__list");
 const startNum = 0; // initial slide index (0 ~ 4)
 const pagination = document.querySelector('.slide_pagination');
 const slideNumber = document.querySelector('.slide__number--bold');
-
-
-
-// 첫번째와 마지막 슬라이드 복제
-let firstChild = slideList.firstElementChild;
-let lastChild = slideList.lastElementChild;
-let clonedFirst = firstChild.cloneNode(true);
-let clonedLast = lastChild.cloneNode(true);
-
-// 복제한 슬라이드 추가
-slideList.appendChild(clonedFirst);
-slideList.insertBefore(clonedLast, slideList.firstElementChild);
-
-slideList.style.transform =
-    'translate3d(-' + slideWidth * (startNum + 1) + 'px, 0px, 0px)';
-
-let curIndex = startNum; // current slide index (except copied slide)
-let curSlide = slideContents[curIndex]; // current slide dom
-curSlide.classList.add('slide_active');
 
 // 숫자 번호 넣기
 let number = slideNumber.innerText;
@@ -121,54 +101,145 @@ function pad(n, width) {
     n = n + '';
     return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
   }
-/** Next Button Event */
-slideBtnNext.addEventListener('click', () => {
-    // curIndex = 현재 인덱스 / cur = 현재 값
-    if (curIndex <= slideLength - 1) {
-        slideList.style.transition = slideSpeed + 'ms';
-        slideList.style.transform =
-          'translate3d(-' + slideWidth * (curIndex + 2) + 'px, 0px, 0px)';
-          console.log(slideWidth * (curIndex + 2));
-          number = pad(curIndex + 2,2);
-      }
-      if (curIndex === slideLength - 1) {
-        setTimeout(function () {
-          slideList.style.transition = '0ms';
-          slideList.style.transform =
-            'translate3d(-' + slideWidth + 'px, 0px, 0px)';
-        }, slideSpeed);
-        curIndex = -1;
-        number = pad(curIndex + 2,2);
-      }
-      curSlide.classList.remove('slide_active');
-      curSlide = slideContents[++curIndex];
-      curSlide.classList.add('slide_active');
-      slideNumber.innerText = number;
-      console.log(slideWidth);
-});
- /** Prev Button Event */
-slideBtnPrev.addEventListener('click', () => {
-    // curIndex = 현재 인덱스 / cur = 현재 값
-    if (curIndex >= 0) {
-        slideList.style.transition = slideSpeed + 'ms';
-        slideList.style.transform =
-        'translate3d(-' + slideWidth * curIndex + 'px, 0px, 0px)';
-        number = pad(curIndex,2);
-    }
-    if (curIndex === 0) {
-        setTimeout(function () {
-          slideList.style.transition = '0ms';
-          slideList.style.transform =
-            'translate3d(-' + slideWidth * slideLength + 'px, 0px, 0px)';
-        }, slideSpeed);
-        curIndex = slideLength;
-        number = pad(curIndex,2);
-    }
-    curSlide.classList.remove('slide_active');
-    curSlide = slideContents[--curIndex];
-    curSlide.classList.add('slide_active');
-    slideNumber.innerText = number;
-});
+
+function prev(){
+  if(curPos > 0){
+    nextBtn.removeAttribute("disabled")
+    console.log(document.querySelector(".slide__box").offsetWidth)
+    // postion += IMAGE_WIDTH;
+    postion += document.querySelector(".slide__box").offsetWidth;
+    images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos - 1;
+  }
+  if(curPos == 0){
+    prevBtn.setAttribute('disabled', 'true')
+  }
+}
+function next(){
+  if(curPos < 1){
+    prevBtn.removeAttribute("disabled")
+    console.log(document.querySelector(".slide__box").offsetWidth)
+    // postion += IMAGE_WIDTH;
+    postion -= document.querySelector(".slide__box").offsetWidth;
+    images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos + 1;
+    number = pad(curPos + 2,2);
+  }
+  if(curPos == 1){
+    nextBtn.setAttribute('disabled', 'true')
+    number = pad(curPos);
+  }
+}
+
+let start_x, end_x;
+
+function touch_start(event) {
+  start_x = event.touches[0].pageX
+}
+
+function touch_end(event) {
+  end_x = event.changedTouches[0].pageX;
+  if(start_x > end_x){
+    next();
+  }else{
+    prev();
+  }
+}
+ 
+function init(){
+  prevBtn.setAttribute('disabled', 'true')
+  prevBtn.addEventListener("click", prev)
+  nextBtn.addEventListener("click", next)
+  
+  images.addEventListener('touchstart', touch_start);
+  images.addEventListener('touchend', touch_end);
+}
+ 
+init();
+// const slideList = document.querySelector('.slide__list');
+// const slideContents = document.querySelectorAll('.slide__section');
+// const slideBtnNext = document.querySelector('.slide__btn--next');
+// const slideBtnPrev = document.querySelector('.slide__btn--prev');
+// const slideLength = slideContents.length;
+// const slideWidth = document.querySelector(".slide__section").offsetWidth;
+// const slideSpeed = 300; // slide speed
+// const startNum = 0; // initial slide index (0 ~ 4)
+// const pagination = document.querySelector('.slide_pagination');
+// const slideNumber = document.querySelector('.slide__number--bold');
+
+
+
+// // 첫번째와 마지막 슬라이드 복제
+// let firstChild = slideList.firstElementChild;
+// let lastChild = slideList.lastElementChild;
+// let clonedFirst = firstChild.cloneNode(true);
+// let clonedLast = lastChild.cloneNode(true);
+
+// // 복제한 슬라이드 추가
+// slideList.appendChild(clonedFirst);
+// slideList.insertBefore(clonedLast, slideList.firstElementChild);
+
+// slideList.style.transform =
+//     'translate3d(-' + slideWidth * (startNum + 1) + 'px, 0px, 0px)';
+
+// let curIndex = startNum; // current slide index (except copied slide)
+// let curSlide = slideContents[curIndex]; // current slide dom
+// curSlide.classList.add('slide_active');
+
+// // 숫자 번호 넣기
+// let number = slideNumber.innerText;
+// //  숫자 앞에 0 추가하기
+// function pad(n, width) {
+//     n = n + '';
+//     return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+//   }
+// /** Next Button Event */
+// slideBtnNext.addEventListener('click', () => {
+//     // curIndex = 현재 인덱스 / cur = 현재 값
+//     if (curIndex <= slideLength - 1) {
+//         slideList.style.transition = slideSpeed + 'ms';
+//         slideList.style.transform =
+//           'translate3d(-' + slideWidth * (curIndex + 2) + 'px, 0px, 0px)';
+//           number = pad(curIndex + 2,2);
+//       }
+//       if (curIndex === slideLength - 1) {
+//         setTimeout(function () {
+//           slideList.style.transition = '0ms';
+//           slideList.style.transform =
+//             'translate3d(-' + slideWidth + 'px, 0px, 0px)';
+//         }, slideSpeed);
+//         curIndex = -1;
+//         number = pad(curIndex + 2,2);
+//       }
+//       curSlide.classList.remove('slide_active');
+//       curSlide = slideContents[++curIndex];
+//       curSlide.classList.add('slide_active');
+//       slideNumber.innerText = number;
+//       console.log(slideWidth);
+// });
+//  /** Prev Button Event */
+// slideBtnPrev.addEventListener('click', () => {
+//     // curIndex = 현재 인덱스 / cur = 현재 값
+//     if (curIndex >= 0) {
+//         slideList.style.transition = slideSpeed + 'ms';
+//         slideList.style.transform =
+//         'translate3d(-' + slideWidth * curIndex + 'px, 0px, 0px)';
+//         number = pad(curIndex,2);
+//     }
+//     if (curIndex === 0) {
+//         setTimeout(function () {
+//           slideList.style.transition = '0ms';
+//           slideList.style.transform =
+//             'translate3d(-' + slideWidth * slideLength + 'px, 0px, 0px)';
+//         }, slideSpeed);
+//         curIndex = slideLength;
+//         number = pad(curIndex,2);
+//     }
+//     curSlide.classList.remove('slide_active');
+//     curSlide = slideContents[--curIndex];
+//     curSlide.classList.add('slide_active');
+//     slideNumber.innerText = number;
+// });
 // 섹션3 - 주요 소프트웨어
 
 const bgImg = document.querySelector('#img__background');
